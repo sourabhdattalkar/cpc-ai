b5c338153df1f7f7ab59b39177366cee1fdc07fc
Mr. Sourabh Dattalkar <sourabh@SKU.LOCAL>
2025-01-08 01:43:47 +0530

    8th commit of AI

--- ai_chat/forms.py
+++ ai_chat/forms.py
@@ -4,4 +4,4 @@ class APIKeyForm(forms.Form):
     api_key = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your API key'}))
 
 class ChatForm(forms.Form):
-    question = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ask a question...'}))
+    question = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ask a question...'}),max_length=1000)
